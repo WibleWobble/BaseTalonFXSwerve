ae1b64281aea15b97120929cf1896c600d3c65c2
Logan Delplanche <Logan.C.Delplanche@gmail.com>
2024-06-11 15:13:28 -0700

    PID Changed to work better:: TESTED:: WORKING

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -137,13 +137,13 @@ public final class Constants {
     //Will have to wait until we build the robot
     public static final class AutoConstants { //TODO: The below constants are used in the example auto, and must be tuned to specific robot
         public static final double kMaxSpeedMetersPerSecond = 3;
-        public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+        public static final double kMaxAccelerationMetersPerSecondSquared = 1.5;
         public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
         public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
     
-        public static final double kPXController = 1;
-        public static final double kPYController = 1;
-        public static final double kPThetaController = 1;
+        public static final double kPXController = 2;
+        public static final double kPYController = 2;
+        public static final double kPThetaController = 2;
     
         /* Constraint for the motion profilied robot angle controller */
         public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
--- src/main/java/frc/robot/autos/exampleAuto.java
+++ src/main/java/frc/robot/autos/exampleAuto.java
@@ -28,9 +28,9 @@ public class exampleAuto extends SequentialCommandGroup {
         // All units in meters.
         Trajectory exampleTrajectory =
             TrajectoryGenerator.generateTrajectory(
-                new Pose2d(0, 0, new Rotation2d(0)),
-                List.of(new Translation2d(2, 0), new Translation2d(2, 2), new Translation2d(0, 2)),
-                new Pose2d(0.1, 0.1, new Rotation2d(0)),
+                new Pose2d(0, 0, new Rotation2d(0.01)),
+                List.of(new Translation2d(0, 2), new Translation2d(2,3),new Translation2d(3,0),new Translation2d(0,2)),
+                new Pose2d(0.01, 0.01, new Rotation2d(Math.PI)),
                 config);
 
         var thetaController =
